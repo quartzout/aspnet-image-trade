e45f484d47290fd3b4bcc823d1b4c1700079d480
quartzout <romatoid@gmail.com>
2022-12-15 13:38:11 +0300

    Переименовал проекты и удалил ненужные

--- UserManager/UserLoginInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UserManager;
-
-#nullable disable
-public class UserLoginInfo
-{
-    public string Email { get; set; }
-    public string Password { get; set; }
-}
--- Webapp174/Models/Identity/UserDbContext.cs
+++
@@ -1,8 +0,0 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using RazorPages.Identity.Classes;
-
-public class UserDbContext : IdentityDbContext<User>
-{
-	public UserDbContext(DbContextOptions<UserDbContext> opts) : base(opts) { }
-}
