6b918185dd4491d0e957bea68b3c0fecc6aec444
quartzout <romatoid@gmail.com>
2023-02-13 10:56:24 +0300

    Добавил модель пользователя для возвращения из Account контроллера

---
+++ API/Models/UserGetDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Models;
+
+#nullable disable
+public class UserGetDto
+{
+    public string Id { get; set; }
+    public string DisplayName { get; set; }
+    public string Email { get; set; }
+    public int CoinBalance { get; set; }
+}
