1080a5465ff1188787aeb05b2cd1958a71211d5e
quartzout <romatoid@gmail.com>
2023-02-13 10:55:13 +0300

    Добавил модели логина и регистрации для JWT Account контроллера

---
+++ API/Models/LoginModelPostJWT.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+#nullable disable
+/// <summary>
+/// Модель, в которую биндятся данные с формы логина пользователя
+/// </summary>
+public class LoginModelPostJWT
+{
+
+    [EmailAddress]
+    [Required]
+    [DisplayName("Email")]
+    public string Email { get; set; }
+
+    [DataType(DataType.Password)]
+    [Required]
+    [DisplayName("Password")]
+    public string Password { get; set; }
+}
---
+++ API/Models/RegisterModelPostJWT.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+/// <summary>
+/// Модель, в которую биндятся данные с формы регистрации пользователя
+/// </summary>
+#nullable disable
+public class RegisterModelPOSTJWT
+{
+    [DisplayName("Email")]
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [DisplayName("Name")]
+    [Required]
+    [StringLength(maximumLength: 20, ErrorMessage = "Name should be less than 20 characters long")]
+    public string DisplayName { get; set; }
+
+    [DisplayName("Password")]
+    [Required]
+    [DataType(DataType.Password)]
+    [StringLength(maximumLength: 100, MinimumLength = 8, ErrorMessage = "Password should take at least 8 characters")]
+    public string Password { get; set; }
+}
