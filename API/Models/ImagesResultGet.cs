ed5b1f0da1d6f1861030b6905c976ba558dbd397
quartzout <romatoid@gmail.com>
2023-02-15 14:29:33 +0300

    добавил модель, возвращаемую из контроллера изображений пользователя

---
+++ API/Models/ImagesResultGet.cs
@@ -0,0 +1,9 @@
+﻿using Mvc.Models;
+
+namespace API.Models
+{
+    public class ImagesResultGet
+    {
+        public IEnumerable<ImageGetDto> Images { get; set; } = new List<ImageGetDto>();
+    }
+}
