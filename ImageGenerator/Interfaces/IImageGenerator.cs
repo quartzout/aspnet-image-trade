fe033e220acc8eb7c4cdb37d02e3479c41678147
quartzout <romatoid@gmail.com>
2023-02-16 08:03:09 +0300

    добавил описание метода

--- ImageGenerator/Interfaces/IImageGenerator.cs
+++ ImageGenerator/Interfaces/IImageGenerator.cs
@@ -5,5 +5,8 @@
 /// </summary>
 public interface IImageGenerator
 {
+	/// <summary>
+	/// Генерирует изображение, сохраняет его в файловой системе и возвращает полный путь к нему
+	/// </summary>
 	public Task<string> GeneratePicture();
 }
